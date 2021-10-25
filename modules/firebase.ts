6616efca734b07d255939028f8ebd38d2d2ed86c
Viacheslav Logvinets <7425816-LanGvest@users.noreply.gitlab.com>
2021-10-25 12:41:22 +0300

    fix firebase 5

--- modules/firebase.ts
+++ modules/firebase.ts
@@ -4,7 +4,7 @@ let configValue: string | null = process.env.FB_CONFIG || process.env.NEXT_PUBLI
 let firebaseApp: FirebaseApp | null = null;
 console.log("###INFO###", process.env.FB_CONFIG, typeof process.env.FB_CONFIG);
 console.log("###INFO###", process.env.NEXT_PUBLIC_FB_CONFIG, typeof process.env.NEXT_PUBLIC_FB_CONFIG);
-console.log("###INFO###", configValue, typeof process.env.configValue);
-if(configValue) firebaseApp = initializeApp(JSON.parse(configValue));
+console.log("###INFO###", configValue, typeof configValue);
+if(configValue) firebaseApp = initializeApp(JSON.parse(configValue.replaceAll("'", "\"")));
 
 export default firebaseApp;
