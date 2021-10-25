6f1566ebc4f65bd3931828cd3149ef1e4349c815
Viacheslav Logvinets <7425816-LanGvest@users.noreply.gitlab.com>
2021-10-25 12:45:07 +0300

    fix firebase 7

--- modules/firebase.ts
+++ modules/firebase.ts
@@ -2,9 +2,9 @@ import {initializeApp, FirebaseApp} from "firebase/app";
 
 let configValue: string | null = process.env.FB_CONFIG || process.env.NEXT_PUBLIC_FB_CONFIG || null;
 let firebaseApp: FirebaseApp | null = null;
-console.log("###INFO###", process.env.FB_CONFIG, typeof process.env.FB_CONFIG);
-console.log("###INFO###", process.env.NEXT_PUBLIC_FB_CONFIG, typeof process.env.NEXT_PUBLIC_FB_CONFIG);
-console.log("###INFO###", configValue, typeof configValue);
+console.log("### INFO ###", process.env.FB_CONFIG, typeof process.env.FB_CONFIG);
+console.log("### INFO ###", process.env.NEXT_PUBLIC_FB_CONFIG, typeof process.env.NEXT_PUBLIC_FB_CONFIG);
+console.log("### INFO ###", configValue, typeof configValue);
 if(configValue) firebaseApp = initializeApp(JSON.parse(configValue.replace(/'/g, "\"")));
 
 export default firebaseApp;
