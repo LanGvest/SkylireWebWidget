5bbbe44bbd0688071bfbf3467cef967f0e4d47e5
Viacheslav Logvinets <7425816-LanGvest@users.noreply.gitlab.com>
2021-10-25 12:35:36 +0300

    fix firebase 3

--- modules/firebase.ts
+++ modules/firebase.ts
@@ -2,6 +2,8 @@ import {initializeApp, FirebaseApp} from "firebase/app";
 
 let configValue: string | null = process.env.FB_CONFIG || process.env.NEXT_PUBLIC_FB_CONFIG || null;
 let firebaseApp: FirebaseApp | null = null;
+console.log(process.env.FB_CONFIG);
+console.log(process.env.NEXT_PUBLIC_FB_CONFIG);
 console.log(configValue);
 if(configValue) firebaseApp = initializeApp(JSON.parse(configValue));
 
