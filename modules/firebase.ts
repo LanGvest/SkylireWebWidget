24b6f963d47933e5da06a282e08002a8f21d8878
Viacheslav Logvinets <7425816-LanGvest@users.noreply.gitlab.com>
2021-10-25 12:39:34 +0300

    fix firebase 4

--- modules/firebase.ts
+++ modules/firebase.ts
@@ -2,9 +2,9 @@ import {initializeApp, FirebaseApp} from "firebase/app";
 
 let configValue: string | null = process.env.FB_CONFIG || process.env.NEXT_PUBLIC_FB_CONFIG || null;
 let firebaseApp: FirebaseApp | null = null;
-console.log(process.env.FB_CONFIG);
-console.log(process.env.NEXT_PUBLIC_FB_CONFIG);
-console.log(configValue);
+console.log("###INFO###", process.env.FB_CONFIG, typeof process.env.FB_CONFIG);
+console.log("###INFO###", process.env.NEXT_PUBLIC_FB_CONFIG, typeof process.env.NEXT_PUBLIC_FB_CONFIG);
+console.log("###INFO###", configValue, typeof process.env.configValue);
 if(configValue) firebaseApp = initializeApp(JSON.parse(configValue));
 
 export default firebaseApp;
